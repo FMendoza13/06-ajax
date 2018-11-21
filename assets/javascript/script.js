16f9bdd293177802700fcb1dbdb2d3fffc615184
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-11-21 13:44:57 -0800

    changed my jquery to uncompressed and thought through and typed pseudocode

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+
+// I hope to create arrays and variable
+
+// create function that creates buttons using elements with the array
+
+// add click event for submit button
+
+// setting up storage session for each movie
+
+// incorporate click event listener on images to connect ajax from  API
+
+// add class to make image into gif when it's clicked upon
